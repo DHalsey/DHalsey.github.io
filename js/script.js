ba58d5bdce51843e4801403c5ccf0c2b75a4c33a
DHalsey <dhalsey@ucsc.edu>
2019-12-17 01:33:43 -0800

    fixed scrolling fade issue

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ if(span){span.onclick = function() {
 $(document).ready(function() {
     $(window).scroll( function(){
         $('.fade').each( function(){
-            var bottom_of_element = $(this).offset().top + $(window).height(); /*+ $(this).outerHeight()/4;*/
+            var bottom_of_element = $(this).offset().top + $(window).height()/6; /*+ $(this).outerHeight()/4;*/
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             if( bottom_of_window > bottom_of_element ){
@@ -44,7 +44,7 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $('.fade').each( function(){
-        var bottom_of_element = $(this).offset().top + $(window).height()/5;/*$(this).outerHeight()/4;*/
+        var bottom_of_element = $(this).offset().top + $(window).height()/6;/*$(this).outerHeight()/4;*/
         var bottom_of_window = $(window).scrollTop() + $(window).height();
 
         if( bottom_of_window > bottom_of_element ){
