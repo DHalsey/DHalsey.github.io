1b4b74ec0132e4d0f09171e8b65c08e1ae25b911
DHalsey <dhalsey@ucsc.edu>
2019-12-17 00:42:00 -0800

    live icon buttons added to index

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ if(span){span.onclick = function() {
 $(document).ready(function() {
     $(window).scroll( function(){
         $('.fade').each( function(){
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight()/4;
+            var bottom_of_element = $(this).offset().top + $(window).height(); /*+ $(this).outerHeight()/4;*/
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             if( bottom_of_window > bottom_of_element ){
@@ -44,15 +44,24 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $('.fade').each( function(){
-        var bottom_of_element = $(this).offset().top + $(this).outerHeight()/4;
+        var bottom_of_element = $(this).offset().top + $(window).height()/5;/*$(this).outerHeight()/4;*/
         var bottom_of_window = $(window).scrollTop() + $(window).height();
 
         if( bottom_of_window > bottom_of_element ){
             $(this).css("opacity", 1);
                 //$(this).css("margin-top", '0px');
-            } else{
-                $(this).css("opacity",0);
-                //$(this).css("margin-top", '100px');
-            }
-        }); 
+        } else{
+            $(this).css("opacity",0);
+            //$(this).css("margin-top", '100px');
+         }
+    }); 
+});
+
+$(".icon-click-background").mouseover(function(){
+    $(this).next().css("clip-path", "circle(" + 100 + "% at center)");
+    $(this).next().parent().parent().css("transform", "scale(1.2)");
+});
+$(".icon-click-background").mouseout(function(){
+    $(this).next().css("clip-path", "circle(" + 0 + "% at center)");
+    $(this).next().parent().parent().css("transform", "scale(1)");
 });
